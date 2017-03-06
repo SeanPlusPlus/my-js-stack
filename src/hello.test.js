f687d08e9dfc756724f438a2e23039bcd7725666
Sean Stephenson <sms@pixar.com>
2017-03-06 14:44:15 -0800

    add all sorts of server side stuff

--- src/hello.test.js
+++
@@ -1,6 +0,0 @@
-import Hello from './hello'
-
-test('Hello.display', () => {
-  const testHello = new Hello('Test')
-  expect(testHello.display()).toBe('Hello Test')
-})
--- src/index.js
+++
@@ -1,8 +0,0 @@
-// @flow
-/* eslint-disable no-console */
-
-import Hello from './hello'
-
-const msg = new Hello('World')
-
-console.log(msg.display())
